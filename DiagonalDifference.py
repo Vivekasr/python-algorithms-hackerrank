75520d6c2f80d39d329099c2b35883ac02bb49fd
Vivek Sharma <31165335+Vivekasr@users.noreply.github.com>
2018-01-13 23:41:03 +0530

    Update DiagonalDifference.py

--- DiagonalDifference.py
+++ DiagonalDifference.py
@@ -22,6 +22,5 @@ for a_i in range(n):
                 a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
                 a.append(a_t)
                 
-result = dia
-gonalDifference(a)
+result = diagonalDifference(a)
 print(result)
